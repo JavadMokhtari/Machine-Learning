4ecb07863346602e96a3c8b8afac16d8a1cca474
javad-mokhtari <javad.mktri@gmail.com>
2022-01-17 23:21:39 +0330

    Code's syntax and functions updated

--- Classic-Algorithms/Kmeans.py
+++ Classic-Algorithms/Kmeans.py
@@ -13,61 +13,107 @@ def euclidean_distance(row1, row2):
     return distance
 
 
-def K_means(dataset, K):
-    dataset_size = dataset.shape[0]
-    indx = np.random.choice(dataset_size, K, replace=False)
-    centers = dataset[indx, :]
-    new_centers = np.empty(centers.shape)
-    cluster_values = np.empty((dataset_size, 1))
-    error_vector = np.empty(K)
-    error = 1.000
-    iteration = 0
-    # print("\n\nFor K = {}:\n".format(K))
+class Kmeans:
+    def __init__(self):
+        self.centers = None
+        self.optimal_k = None
+        self.clustered = None
+        self.clusters_variance = None
 
-    while error > 0.005:
-        for i in range(dataset_size):
-            min_dist = float('inf')
-            for j in range(K):
-                dist = euclidean_distance(dataset[i], centers[j, :])
-                if dist <= min_dist:
-                    min_dist = dist
-                    cluster_values[i, :] = j
-        # Set a cluster to each dataset row
-        clustered_dataset = np.column_stack((dataset, cluster_values))
+    def K_means(self, dataset, K):
+        dataset_size = dataset.shape[0]
+        indx = np.random.choice(dataset_size, K, replace=False)
+        self.centers = dataset[indx, :]
+        new_centers = np.empty(self.centers.shape)
+        self.clusters_variance = np.ones((self.centers.shape[0], 1))
+        cluster_values = np.empty((dataset_size, 1))
+        error_vector = np.empty(K)
+        error = 1.000
+        iteration = 0
+        # print("\n\nFor K = {}:\n".format(K))
 
-        # Updating centers
-        for i in range(K):
-            cluster = clustered_dataset[np.where(clustered_dataset[:, -1] == i)]
+        while error > 0.005:
+            for i in range(dataset_size):
+                min_dist = float('inf')
+                for j in range(K):
+                    dist = euclidean_distance(dataset[i], self.centers[j, :])
+                    if dist <= min_dist:
+                        min_dist = dist
+                        cluster_values[i, :] = j
+            # Set a cluster to each dataset row
+            clustered_dataset = np.column_stack((dataset, cluster_values))
 
-            if cluster.shape[0] == 0:
-                indx = np.random.randint(dataset_size)
-                new_center = dataset[indx, :]
-            else:
-                column_values = [cluster[:, i] for i in range(cluster.shape[1] - 1)]
-                new_center = np.array(
-                    [np.sum(column_values[i]) / cluster.shape[0] for i in range(cluster.shape[1] - 1)])
+            # Updating centers
+            for i in range(K):
+                cluster = clustered_dataset[np.where(clustered_dataset[:, -1] == i)]
+                if cluster.shape[0] == 0:
+                    indx = np.random.randint(dataset_size)
+                    new_center = dataset[indx, :]
+                else:
+                    column_values = [cluster[:, i] for i in range(cluster.shape[1] - 1)]
+                    new_center = np.array([np.sum(column_values[i]) / cluster.shape[0]
+                                           for i in range(cluster.shape[1] - 1)])
+                    distances = [euclidean_distance(cluster[i, :-1], new_center) for i in range(cluster.shape[0])]
+                    self.clusters_variance[i][0] = sum(distances) / len(distances)
+                new_centers[i, :] = new_center
 
-            new_centers[i, :] = new_center
+            # Calculating distance between new centers and previous centers
+            for i in range(K):
+                error_vector[i] = euclidean_distance(new_centers[i, :], self.centers[i, :])
+            error = sqrt(np.dot(error_vector, error_vector))
 
-        # Calculating distance between new centers and previous centers
-        for i in range(K):
-            error_vector[i] = euclidean_distance(new_centers[i, :], centers[i, :])
-        error = sqrt(np.dot(error_vector, error_vector))
+            iteration += 1
+            # print("Relocation of centers in iteration {:2d}: {:.5f}".format(iteration, error))
+            self.centers = np.copy(new_centers)
 
-        iteration += 1
-        # print("Relocation of centers in iteration {:2d}: {:.5f}".format(iteration, error))
-        centers = np.copy(new_centers)
+        # print('\n\n', 'The obtained centers are:', '\n\n', np.around(centers, decimals=2))
+        self.clustered = clustered_dataset
 
-    # print('\n\n', 'The obtained centers are:', '\n\n', np.around(centers, decimals=2))
-    return clustered_dataset
+    def find_optimal_K(self, input_dataset, max_K=10, iteration=30):
+        K_values = list()
+        for _iter in range(iteration):
+            RMSSTD_values = list()
+            for K in range(1, max_K):
+                self.K_means(input_dataset, K)
+                clustered = self.clustered
+
+                # Creating a list of clusters for CVI functions input
+                clusters_list = list()
+                for c in range(K):
+                    cluster = clustered[np.where(clustered[:, -1] == c)]
+                    clusters_list.append(cluster)
+
+                # Getting RMSSTD index value
+                cvi = RMSSTD(clusters_list)
+                rmsstd = cvi.RMDDTD_index()
+                RMSSTD_values.append(rmsstd)
+
+            for i in range(1, len(RMSSTD_values)):
+                if RMSSTD_values[i-1]/RMSSTD_values[i] <= 1.1:
+                    optimal_K = i
+                    break
+            K_values.append(optimal_K)
+
+        # Setting figure of RMSSTD values by number of clusters
+        # plt.plot([i for i in range(1, max_K)], RMSSTD_values)
+        # plt.xlabel("Number of clusters")
+        # plt.ylabel("RMSSTD")
+        # plt.savefig('../Results/Elbow_method_figure.jpg')
+        # plt.show()
+
+        return max(set(K_values), key=K_values.count)
 
 
 def main():
     # import iris data and clustering
     input_data = np.loadtxt('../Data/iris/iris_train.csv', delimiter=',')
 
-    k = 3
-    iris_clustered = K_means(input_data, k)
+    kmeans = Kmeans()
+    optimal_K = kmeans.find_optimal_K(input_data)
+    print("\nThe optimal K was found by RMSSTD index:\nK = {}".format(optimal_K))
+    k = optimal_K
+    kmeans.K_means(input_data, k)
+    iris_clustered = kmeans.clustered
 
     # Creating a list of clusters for CVI functions input
     clusters_list = list()
@@ -78,17 +124,14 @@ def main():
     # Getting Dunn's index value
     cvi = Dunn(clusters_list)
     Dunn_value = cvi.Dun_index()
+    print('\n\n', "Dunn's Index = {:.2f}".format(Dunn_value), sep='')
 
     # Getting Davies-Bouldin index value
     cvi = Davies_Bouldin(clusters_list)
     DB_value = cvi.Davies_Bouldin_index()
+    print("Davies-Bouldin Index = {:.2f}".format(DB_value))
 
-    # I consider an index that is obtained by dividing the Dunn's index by Davies-Bouldin index
-    my_index = Dunn_value / DB_value
-    print('\n\n', "Clustering Validity Index Value is: {:.2f}\n".format(my_index), sep='')
-    print("This index is obtained by dividing the Dunn's index by Davies-Bouldin index")
-
-    # visualization of clustering result
+    # Visualization of clustering result
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111, projection='3d')
 
--- Classic-Algorithms/RadialBasisFunction.py
+++ Classic-Algorithms/RadialBasisFunction.py
@@ -1,11 +1,12 @@
 import numpy as np
-import importlib
 from math import sqrt
-FindOptimal_K = importlib.import_module('FindOptimal-K-means', '*')
+from Kmeans import Kmeans
 
 
 # Is used as kernel function in input layer of RBF
-def Gaussian(r, var=1):
+def Gaussian(r, var):
+    if var is None:
+        var = 1
     return np.exp(- (r ** 2) / (2 * var))
 
 
@@ -25,71 +26,32 @@ def euclidean_distance(row1, row2):
 
 class RadialBasisFunction:
     # Initialization function
-    def __init__(self, dataset, num_input=4, num_output=3, learning_rate=0.01, epoch=2000):
+    def __init__(self, dataset, num_input=4, num_output=3, learning_rate=0.01, epoch=1000):
         self.dataset = dataset
         self.num_input = num_input
         self.num_output = num_output
 
-        find_optimal_K = FindOptimal_K.__getattribute__('find_optimal_K')
-        self.num_hidden = find_optimal_K(self.dataset)
+        kmeans = Kmeans()
+        self.num_hidden = kmeans.find_optimal_K(self.dataset)
         print("\nThe optimal number of hidden neurons was found:\nK = {}\n".format(self.num_hidden))
 
         self.learning_rate = learning_rate
         self.epoch = epoch
         self.output_layer_weights = np.random.uniform(-2, 2, (self.num_hidden, self.num_output))
         self.output_layer_biases = np.random.uniform(-2, 2, (1, self.num_output))
-        self.centers = self.K_means_centers(self.dataset[:, :-1])
         self.hidden_neurons = np.empty((1, self.num_hidden))
 
-    def K_means_centers(self, dataset):
-        dataset_size = dataset.shape[0]
-        indx = np.random.choice(dataset_size, self.num_hidden, replace=False)
-        centers = dataset[indx, :]
-        new_centers = np.empty(centers.shape)
-        cluster_values = np.empty((dataset_size, 1))
-        error_vector = np.empty(self.num_hidden)
-        error = 1.000
-        iteration = 0
-
-        while error > 0.005:
-            for i in range(dataset_size):
-                min_dist = float('inf')
-                for j in range(self.num_hidden):
-                    dist = euclidean_distance(dataset[i], centers[j, :])
-                    if dist <= min_dist:
-                        min_dist = dist
-                        cluster_values[i, :] = j
-
-            # Set a cluster to each dataset row
-            clustered_dataset = np.column_stack((dataset, cluster_values))
-            # Updating centers
-            for i in range(self.num_hidden):
-                cluster = clustered_dataset[np.where(clustered_dataset[:, -1] == i)]
-
-                if cluster.shape[0] == 0:
-                    indx = np.random.randint(dataset_size)
-                    new_center = dataset[indx, :]
-                else:
-                    column_values = [cluster[:, i] for i in range(cluster.shape[1] - 1)]
-                    new_center = np.array([np.sum(column_values[i]) / cluster.shape[0]
-                                           for i in range(cluster.shape[1] - 1)])
-                new_centers[i, :] = new_center
-
-            # Calculating distance between new centers and previous centers
-            for i in range(self.num_hidden):
-                error_vector[i] = euclidean_distance(new_centers[i, :], centers[i, :])
-            error = sqrt(np.dot(error_vector, error_vector))
-
-            iteration += 1
-            centers = np.copy(new_centers)
-        return centers
+        kmeans.K_means(self.dataset, self.num_hidden)
+        self.centers = kmeans.centers
+        self.spreads = kmeans.clusters_variance
 
     def train(self, dataset):
         for epoch in range(self.epoch):
             for row in dataset:
                 for i in range(self.num_hidden):
                     r = euclidean_distance(row[:-1], self.centers[i, :])
-                    self.hidden_neurons[0][i] = Gaussian(r)
+                    sig = self.spreads[i][0]
+                    self.hidden_neurons[0][i] = Gaussian(r, sig)
 
                 # Calculate error in output layer for use in delta rule
                 output = sigmoid(np.dot(self.hidden_neurons, self.output_layer_weights) + self.output_layer_biases)
@@ -104,12 +66,14 @@ class RadialBasisFunction:
     def predict_classification(self, data_row):
         for i in range(self.num_hidden):
             r = euclidean_distance(data_row, self.centers[i, :])
-            self.hidden_neurons[0][i] = Gaussian(r)
+            sig = self.spreads[i][0]
+            self.hidden_neurons[0][i] = Gaussian(r, sig)
         output = sigmoid(np.dot(self.hidden_neurons, self.output_layer_weights) + self.output_layer_biases)
         predict = list(map(round, output[0]))
         for p in predict:
             if p == 1:
                 return predict.index(p)
+        return 0
 
     # Make prediction and measure algorithm performance
     def calculate_performance(self, data_in, data_out):
@@ -133,6 +97,7 @@ class RadialBasisFunction:
             self.train(train_data_clone)
             accuracy = self.calculate_performance(validation_data[i][:, :-1], validation_data[i][:, -1])
             accuracy_list.append(accuracy)
+            print("{:.2f}%".format(accuracy * 100))
 
         performance = sum(accuracy_list)*100/k
         return performance
@@ -144,26 +109,26 @@ def main():
     output_data = np.loadtxt('../Data/iris/iris_train_label.csv', delimiter=',')
     train_data = np.column_stack((input_data, output_data))
 
-    data_test_in = np.loadtxt('../Data/iris/iris_test.csv', delimiter=',')
-    data_test_out = np.loadtxt('../Data/iris/iris_test_label.csv', delimiter=',')
+    input_test_data = np.loadtxt('../Data/iris/iris_test.csv', delimiter=',')
+    output_test_data = np.loadtxt('../Data/iris/iris_test_label.csv', delimiter=',')
 
     rbf = RadialBasisFunction(train_data)
     rbf.train(train_data)
 
     correct = 0
-    for i in range(data_test_out.shape[0]):
-        prediction = rbf.predict_classification(data_test_in[i])
-        real_value = data_test_out[i]
-        print('For {} Expected {}, Got {}.'.format(data_test_in[i], real_value, prediction), end='  ')
+    for i in range(output_test_data.shape[0]):
+        prediction = rbf.predict_classification(input_test_data[i])
+        real_value = output_test_data[i]
+        print('For {} Expected {}, Got {}.'.format(input_test_data[i], real_value, prediction), end='  ')
         if real_value == prediction:
             correct += 1
             print('True')
         else:
             print('False')
-    performance = correct / data_test_out.shape[0]
+    performance = correct / output_test_data.shape[0]
     print("\nAccuracy on test dataset: {:.2f}%".format(performance * 100))
-    print('\nPlease wait ...\n')
-    print('Performance with K fold validation:\n{:.2f}%'.format(rbf.K_fold_validation(train_data)))
+    print('\nPerformance with K fold validation:\n')
+    print('\nAverage = {:.2f}%'.format(rbf.K_fold_validation(train_data)))
 
 
 if __name__ == "__main__":
